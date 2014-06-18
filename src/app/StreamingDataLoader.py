4f3634c51a8d7d65ca4f68f561c3708ea7b5b5a9
sreeder <stephanie.reeder@usu.edu>
2014-06-18 15:41:41 -0600

    outline todo list for the loader

--- src/app/StreamingDataLoader.py
+++ src/app/StreamingDataLoader.py
@@ -6,7 +6,9 @@ __author__ = 'Jacob'
 """ Loader """
 
 from argparse import ArgumentParser
+
 from src.loader.models.loaderModel import LoaderModel
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="StreamDataLoader")
@@ -19,6 +21,38 @@ if __name__ == "__main__":
         print "dataFile: ", args.dataFile
 
     model = LoaderModel(args)
+    #read jsonfile
+    #loop through files
+        #canirun()
+        #createpandatable(datafile)
+        # object namedtuple
+        #set up db connection
+        #loop through Mappings
+            # loop through object.DataSeriesMapping
+            # get max(localdatetime) for seriesmapping
+            # TODO: change csvHandler to read entire csv then add another function that will return a specified column after a datetime
+            # get panda column (DataseriesMapping.columnname, localdatetime )
+            #does this series exist?
+                #if no return error
+            #createmapping(DataSeriesMapping, namedtuple, pandacolumn)
+            #savemapping
+            #update value count in the Results Table # if save is successful
+
+    #createresult
+        #Result
+        #TimeSeriesResult
+        # TODO create the result and timeSeriesResult in the wizard and pass the resultid we will assume the user has already created the series
+
+
+
+
+    #createmapping
+        # get TimeSeriesResultValue create skeleton(template)
+        #fill in the template with info from the DAtaSEriesMapping
+
+
+
+
 
 
 
