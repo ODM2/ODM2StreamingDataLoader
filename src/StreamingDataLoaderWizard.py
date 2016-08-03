fda0eb51a3e9195ac1e0dae52d1b610fc067c5ef
Mikaila Young <young.mikaila@gmail.com>
2016-08-03 14:32:36 -0600

    update to streamingdataloaderwizard file

--- src/StreamingDataLoaderWizard.py
+++ src/StreamingDataLoaderWizard.py
@@ -1,3 +1,20 @@
+# import os
+# import sys
+
+# import ObjectListView
+# import pyodbc
+# import pymysql
+# import sqlite3
+# sys.path.insert(0,os.path.dirname(os.path.dirname(__file__)))
+# from src.wizard.controller.frmMain import MainController
+# import wx
+# if __name__ == '__main__':
+    # app = wx.App()
+    # frame = MainController(None)
+    # frame.CenterOnScreen()
+    # frame.Show()
+    # app.MainLoop()
+
 import os
 import sys
 
@@ -5,7 +22,12 @@ import ObjectListView
 import pyodbc
 import pymysql
 import sqlite3
-sys.path.insert(0,os.path.dirname(os.path.dirname(__file__)))
+
+
+src_folder = os.path.dirname(os.path.abspath(__file__))
+root_folder = os.path.dirname(src_folder)
+sys.path.insert(0, root_folder)
+
 from src.wizard.controller.frmMain import MainController
 import wx
 if __name__ == '__main__':
@@ -14,4 +36,3 @@ if __name__ == '__main__':
     frame.CenterOnScreen()
     frame.Show()
     app.MainLoop()
-
