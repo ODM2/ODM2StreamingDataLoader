fb144a0f5a688fdacef3b0aacc8efd7ebf7af11c
sreeder <stephanie.reeder@usu.edu>
2017-06-15 15:44:30 -0600

    fix merge commit

--- src/wizard/controller/frmAddNewSampFeatPanel.py
+++ src/wizard/controller/frmAddNewSampFeatPanel.py
@@ -71,51 +71,37 @@ class AddNewSampFeatPanelController(AddNewSampFeatPanelView):
             # Create the variable in the database.
             write = self.db.getWriteSession()
 
-            site = Sites(
-                SamplingFeatureUUID=str(self._uuid),
-                SamplingFeatureCode=self.requiredValues['code'],
-                SamplingFeatureTypeCV=self.requiredValues['vType'],
-                SamplingFeatureName=self.optionalValues['name'],
-                SamplingFeatureDescription=self.optionalValues['desc'],
-                SamplingFeatureGeotypeCV=self.optionalValues['geoType'],
-                Elevation_m=self.optionalValues['elevation'],
-                ElevationDatumCV=self.optionalValues['elevationDatum'],
-                FeatureGeometry=self.optionalValues['featureGeo'],
-                SpatialReferenceID=self.requiredValues['spatialRef'],
-                SiteTypeCV=self.requiredValues['siteType'],
-                Latitude=self.requiredValues['lat'],
-                Longitude=self.requiredValues['long']
-            )
 
-                site = Sites(SamplingFeatureUUID=str(self._uuid),
-                            SamplingFeatureCode=self.requiredValues['code'],
-                            SamplingFeatureTypeCV=self.requiredValues['vType'],
-                            SamplingFeatureName=self.optionalValues['name'],
-                            SamplingFeatureDescription=self.optionalValues['desc'],
-                            SamplingFeatureGeotypeCV=self.optionalValues['geoType'],
-                            Elevation_m=self.optionalValues['elevation'],
-                            ElevationDatumCV=self.optionalValues['elevationDatum'],
-                            FeatureGeometryWKT=self.optionalValues['featureGeo'],
-                            SpatialReferenceID=self.requiredValues['spatialRef'],
-                            SiteTypeCV=self.requiredValues['siteType'],
-                            Latitude=self.requiredValues['lat'],
-                            Longitude=self.requiredValues['long'])
-                # site = write.createSite(site)
-                self.sf = write.createSamplingFeature(site)
+
+            site = Sites(SamplingFeatureUUID=str(self._uuid),
+                        SamplingFeatureCode=self.requiredValues['code'],
+                        SamplingFeatureTypeCV=self.requiredValues['vType'],
+                        SamplingFeatureName=self.optionalValues['name'],
+                        SamplingFeatureDescription=self.optionalValues['desc'],
+                        SamplingFeatureGeotypeCV=self.optionalValues['geoType'],
+                        Elevation_m=self.optionalValues['elevation'],
+                        ElevationDatumCV=self.optionalValues['elevationDatum'],
+                        FeatureGeometryWKT=self.optionalValues['featureGeo'],
+                        SpatialReferenceID=self.requiredValues['spatialRef'],
+                        SiteTypeCV=self.requiredValues['siteType'],
+                        Latitude=self.requiredValues['lat'],
+                        Longitude=self.requiredValues['long'])
+            # site = write.createSite(site)
+            self.sf = write.createSamplingFeature(site)
             self.sampling_feature = site
 
-        except Exception as error:
-            print error
+        # except Exception as error:
+        #     print error
 
-                self.parent.parent.list_ctrl.SetObjects(self.parent.parent.getSeriesData())
-                length = self.parent.parent.list_ctrl.GetItemCount.im_self.ItemCount
-                length = length - 1
-                self.parent.parent.list_ctrl.Focus(length)
-                self.parent.parent.list_ctrl.Select(length, 1)###boop
-                #####update list and select here
+            self.parent.parent.list_ctrl.SetObjects(self.parent.parent.getSeriesData())
+            length = self.parent.parent.list_ctrl.GetItemCount.im_self.ItemCount
+            length = length - 1
+            self.parent.parent.list_ctrl.Focus(length)
+            self.parent.parent.list_ctrl.Select(length, 1)###boop
+            #####update list and select here
 
-            except Exception as e:
-                print e
+        except Exception as e:
+            print e
 
         event.Skip()
 
