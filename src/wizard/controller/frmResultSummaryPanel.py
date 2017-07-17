1bc31562c2c1e1808128f09385c26f8062b09dc5
Mikaila Young <young.mikaila@gmail.com>
2017-07-17 14:19:52 -0600

    Removed time units from result page panel

--- src/wizard/controller/frmResultSummaryPanel.py
+++ src/wizard/controller/frmResultSummaryPanel.py
@@ -211,12 +211,12 @@ class ResultSummaryPanel(ResultPageView):
         if str(self.comboSR.GetStringSelection()) != "":
             sr = d[self.comboSR.GetStringSelection()]
 
-        timeSpacing = None
-        if str(self.txtIntended.GetValue()) != "":
-            timeSpacing = float(self.txtIntended.GetValue())
-        timeUnit = None
-        if str(self.comboIntendedUnits.GetStringSelection()) != "":
-            timeUnit = lunitsdict[str(self.comboIntendedUnits.GetStringSelection())]
+        # timeSpacing = None
+        # if str(self.txtIntended.GetValue()) != "":
+        #     timeSpacing = float(self.txtIntended.GetValue())
+        # timeUnit = None
+        # if str(self.comboIntendedUnits.GetStringSelection()) != "":
+        #     timeUnit = lunitsdict[str(self.comboIntendedUnits.GetStringSelection())]
 
         print "X", x
         print "Y", y
@@ -242,9 +242,9 @@ class ResultSummaryPanel(ResultPageView):
                                 YLocationUnitsID=yUnit,
                                 ZLocation=z,
                                 ZLocationUnitsID=zUnit,
-                                SpatialReferenceID=sr,
-                                IntendedTimeSpacing=timeSpacing,
-                                IntendedTimeSpacingUnitsID=timeUnit)
+                                SpatialReferenceID=sr)
+                                # IntendedTimeSpacing=timeSpacing,
+                                # IntendedTimeSpacingUnitsID=timeUnit)
         result = write.createResult(tsr)
 
         print result
@@ -306,7 +306,7 @@ class ResultSummaryPanel(ResultPageView):
         self.comboXUnits.AppendItems(length_units_name)
         self.comboYUnits.AppendItems(length_units_name)
         self.comboZUnits.AppendItems(length_units_name)
-        self.comboIntendedUnits.AppendItems(time_units_name)
+        #self.comboIntendedUnits.AppendItems(time_units_name)
         
         self.tax = [{i.TaxonomicClassifierName:i.TaxonomicClassifierID}\
             for i in self.read_session.getTaxonomicClassifiers()]
